d6a38642bcfde9f5f1d7d2a21920551eba8a31ba
Елизавета Панова <panova.ei@edu.spbstu.ru>
2020-05-27 16:36:55 +0300

    .

--- arcanoid.cpp
+++ arcanoid.cpp
@@ -1,18 +1,26 @@
 #include "arcanoid.h"
-
-Arcanoid::Arcanoid(QWidget* parent) {
-  startGame();
-  repaint();
-}
+#include <stdio.h>
+#include <stdlib.h>
 
 void Arcanoid::paintEvent(QPaintEvent* event) {
   Q_UNUSED(event);
   int i, j;
+  char message[30];
   QPainter painter(this);
+  if (lives == 0) {
+    snprintf(message, sizeof(message), "fail");
+    killTimer(timerId);
+  }
+  else if(briksleft == 0) {
+    snprintf(message, sizeof(message), "win");
+    killTimer(timerId);
+  }
+  else 
+    snprintf(message, sizeof(message), "Points: %d\n Lives: %d", points, lives);
   painter.setPen(QColor("#FF8C00"));
   paddle->draw(&painter);
   if (!balls.empty())
-    for (int i = 0; i < balls.size(); i++)
+    for (i = 0; i < balls.size(); i++)
       balls[i]->draw(&painter);
   if (!bonuses.empty())
     for (i = 0; i < bonuses.size(); i++)
@@ -22,13 +30,28 @@ void Arcanoid::paintEvent(QPaintEvent* event) {
       movingBricks[i]->draw(&painter);
   for (i = 0; i < columns; i++)
     for (j = 0; j < rows; j++)
-      if(briks[i][j] != NULL)
-      briks[i][j]->draw(&painter);
+      if (briks[i][j] != NULL)
+        briks[i][j]->draw(&painter);
+  if (!dno.empty()) {
+    tick--;
+    for (i = 0; i < dno.size(); i++)
+      dno[i]->draw(&painter);
+    if (tick == 0) {
+      dno.clear();
+    }
+  }
   catchBonuses();
+  QFont font("Courier", 15, QFont::DemiBold);
+  QFontMetrics fm(font);
+  painter.setFont(font);
+  int textWidth = fm.width(QString(message));
+  painter.translate(QPoint(textWidth, fm.height()));
+  painter.drawText(-textWidth, 0, message);
 }
 
-void Arcanoid::startGame() {
+Arcanoid::Arcanoid(QWidget* parent) {
   int i, j;
+  timerId = startTimer(8);
   for (i = 0; i < columns; i++) {
     for (j = 0; j < 2; j++) {
         briks[i][j] = new SimpleBrick(1, brickw * (2 + i), brickh * (4 + j), brickw, brickh);
@@ -52,7 +75,14 @@ void Arcanoid::startGame() {
   }
   balls.push_back(new Ball(width / 2 - ballD/2, height - 46, ballD));
   paddle = new Paddle(width / 2 - 60, height - 30, 120, 10, width);
+  stickball = balls[0];
   brickPos = 0;
+  bonuslives = 0;
+  lives = 3;
+  points = 0;
+  stick = 1;
+  briksleft = columns * rows - 4;
+  repaint();
 }
 
 void Arcanoid::timerEvent(QTimerEvent* e) {
@@ -70,7 +100,10 @@ void Arcanoid::timerEvent(QTimerEvent* e) {
     }
   if (!balls.empty()) {
     for (int i = 0; i < balls.size(); i++) {
-      balls[i]->move();
+      if (balls[i] == stickball)
+        balls[i]->setX(balls[i]->left() + paddle->getDx());
+      else
+        balls[i]->move();
       checkColisions(balls[i]);
     }
     checkEncounter();
@@ -82,7 +115,7 @@ void Arcanoid::checkEncounter() {
   int i, j;
   for (i = 0; i < balls.size() - 1; i++)
     for (j = i + 1; j < balls.size(); j++)
-      balls[i]->encounter(balls[j]);
+      encounter(balls[i], balls[j]);
 }
 
 void Arcanoid::checkColisions(Ball* ball) {
@@ -96,8 +129,21 @@ void Arcanoid::touchedBox(Ball* ball) {
     ball->setY(0);
     ball->changeYdir();
   }
-  if (ball->bottom() >= height) {
-    ball->changeYdir();
+  if (ball->top() >= height) {
+    int i;
+    for (i = 0; i < balls.size(); i++)
+      if (balls[i] == ball)
+        break;
+    delete ball;
+    balls.erase(balls.begin() + i);
+    if (balls.empty()) {
+      lives--;
+      if (lives > 0) {
+        balls.push_back(new Ball(paddle->left() + (paddle->right() - paddle->left()) / 2 - ballD / 2, paddle->top() - ballD, ballD));
+        stick = 1;
+        stickball = balls[balls.size() - 1];
+      }
+    }
   }
   if ((ball->left() <= 0)) {
     ball->setX(0);
@@ -112,15 +158,26 @@ void Arcanoid::touchedBox(Ball* ball) {
 void Arcanoid::touchedPaddle(Ball* ball) {
   int r = paddle->right();
   int l = paddle->left();
-  if (ball->bottom() >= paddle->top() && ball->left() >= l && ball->right() <= r) {
-    if (ball->right() <= l + (r - l) / 3)
-      ball->moveLeft();
-    else if (ball->left() >= r - (r - l) / 3)
-      ball->moveRight();
-    else
-      ball->changeXdir();
-    ball->setY(paddle->top() - ballD);
-    ball->changeYdir();
+  if (ball->bottom() >= paddle->top()) {
+    if (ball->left() >= l && ball->right() <= r) {
+      if (ball->right() <= l + (r - l) / 3)
+        ball->moveLeft();
+      else if (ball->left() >= r - (r - l) / 3)
+        ball->moveRight();
+      else
+        ball->changeXdir();
+      ball->setY(paddle->top() - ballD);
+      ball->changeYdir();
+      if (stick && stickball == NULL)
+        stickball = ball;
+    }
+    else if (bonuslives) {
+      tick = 10;
+      dno.push_back(new Paddle(ball->left(), paddle->top(), ballD, paddleh, width));
+      repaint();
+      ball->changeYdir();
+      bonuslives--;
+    }
   }
 }
 
@@ -133,13 +190,16 @@ void Arcanoid::touchedBricks(Ball* ball) {
       if (briks[i][j] != NULL) {
         if (touchedLR(ball, briks[i][j])) {
           dropBonus(i, j, briks[i][j]->loseLive(), ball);
+          points++;
           lrTouch = true;
         }
         else if (touchedUD(ball, briks[i][j])) {
           dropBonus(i, j, briks[i][j]->loseLive(), ball);
+          points++;
           udTouch = true;
         }
         if (!briks[i][j]->isAlive()) {
+          briksleft--;
           delete briks[i][j];
           briks[i][j] = NULL;
         }
@@ -149,10 +209,12 @@ void Arcanoid::touchedBricks(Ball* ball) {
     for (i = 0; i < movingBricks.size(); i++) {
       if (touchedLR(ball, movingBricks[i])) {
         movingBricks[i]->loseLive();
+        points++;
         lrTouch = true;
       }
       else if (touchedUD(ball, movingBricks[i])) {
         movingBricks[i]->loseLive();
+        points++;
         udTouch = true;
       }
       if (!movingBricks[i]->isAlive()) {
@@ -169,9 +231,11 @@ void Arcanoid::touchedBricks(Ball* ball) {
 void Arcanoid::dropBonus(int i, int j, Drop bonus, Ball* ball) {
   if (bonus == Drop::nothing)
     return;
+  else if (bonus == Drop::losepoints)
+    points--;
   else if (bonus == Drop::speed)
     ball->speedUp();
-  else
+  else 
     bonuses.push_back(new Bonus(bonus, (2 + i) * brickw, (4 + j) * brickh));
 }
 
@@ -191,6 +255,21 @@ void Arcanoid::catchBonuses() {
         y = 4 + brickPos % columns;
         movingBricks.push_back(new MovingBrick(x, y * brickh, brickw, brickh, brickPos));
         brickPos++;
+      }
+      if (bonuses[i]->getType() == Drop::dropSpeed) {
+        balls[balls.size() - 1]->speedUp();
+      }
+      if (bonuses[i]->getType() == Drop::shrink) {
+        paddle->changeSize(-1);
+      }
+      if (bonuses[i]->getType() == Drop::expand) {
+        paddle->changeSize(1);
+      }
+      if (bonuses[i]->getType() == Drop::live) {
+        bonuslives++;
+      }
+      if (bonuses[i]->getType() == Drop::stick) {
+        stick++;
       }
       delete bonuses[i];
       bonuses.erase(bonuses.begin() + i--);
@@ -234,7 +313,10 @@ void Arcanoid::keyPressEvent(QKeyEvent* e) {
 
   switch (e->key()) {
   case Qt::Key_Space:
-    timerId = startTimer(8);
+    if (stickball != NULL) {
+      stick--;
+      stickball = NULL;
+    }
     break;
   case Qt::Key_Left:
     paddle->moveLeft();
@@ -255,27 +337,49 @@ void Arcanoid::keyReleaseEvent(QKeyEvent * e) {
   }
 }
 
-void Ball::encounter(Ball* b) {
-  Ball* a = this;
-  int xd = abs(a->x - b->x);
-  int yd = abs(a->y - b->y);
-  int tx, ty;
-  if (xd <= d && yd <= d) {
+void Arcanoid::encounter(Ball* a, Ball* b) {
+  int xd = a->distanseX(b);
+  int yd = a->distanseY(b);
+  if (xd <= ballD && yd <= ballD) {
+    a->swapSpeed(b);
+    if (a != stickball && b != stickball)
+      a->split(b);
+    else {
+      a->goUp();
+      b->goUp();
+    }
+  }
+}
+
+int Ball::distanseX(Ball* b) {
+  return abs(this->x - b->x);
+}
+
+int Ball::distanseY(Ball* b) {
+  return abs(this->y - b->x);
+}
+
+void Ball::swapSpeed(Ball* b) {
+    Ball* a = this;
+    int tx, ty;
     tx = a->dx;
     ty = a->dy;
     a->dx = b->dx;
     a->dy = b->dy;
     b->dx = tx;
     b->dy = ty;
-    if (b->x < a->x)
-      a->x = b->x + d;
-    else
-      b->x = a->x + d;
-    if (b->y < a->y)
-      a->y = b->y + d;
-    else
-      b->y = a->y + d;
-  }
+}
+
+void Ball::split(Ball* b) {
+  Ball* a = this;
+  if (b->x < a->x)
+    a->x = b->x + d;
+  else
+    b->x = a->x + d;
+  if (b->y < a->y)
+    a->y = b->y + d;
+  else
+    b->y = a->y + d;
 }
 
 void Arcanoid::checkBrick(MovingBrick* brick) {
--- arcanoid.h
+++ arcanoid.h
@@ -5,10 +5,11 @@
 #include <QKeyEvent>
 #include "ui_arcanoid.h"
 #include <vector>
+#include "brick.h"
 
 const int rows = 8;
 const int columns = 6;
-enum class Drop {nothing, point, speed, newball, brick};
+
 
 class Ball {
 public:
@@ -26,7 +27,7 @@ public:
     dx = -ball.dx;
     dy = -ball.dy;
     d = ball.d;
-    speed = 3;
+    speed = 1;
   }
   void move() {
     x += dx * speed;
@@ -51,6 +52,9 @@ public:
   void moveLeft() {
     dx = -1;
   }
+  void goUp() {
+    dy = -1;
+  }
   void setX(int X) {
     x = X;
   }
@@ -69,7 +73,10 @@ public:
   int right() {
     return x + d;
   }
-  void encounter(Ball* b);
+  void swapSpeed(Ball* b);
+  void split(Ball* b);
+  int distanseX(Ball* b);
+  int distanseY(Ball* b);
 private :
   int speed;
   int d;
@@ -87,6 +94,15 @@ public:
     maxRight = right;
     dx = 0;
   }
+  void changeSize(int sign) {
+    w += sign * shrinkTo;
+    x -= sign * shrinkTo / 2;
+    if (x < 0) {
+      x = 0;
+    }
+    if (x > maxRight - w)
+      x = maxRight - w;
+  }
   void draw(QPainter* qp) {
     qp->setBrush(QColor("#800000"));
     qp->drawRect(x, y, w, h);
@@ -96,6 +112,9 @@ public:
   }
   void moveLeft() {
     dx = -8;
+  }
+  int getDx() {
+    return dx;
   }
   void move() {
     int s = x + dx;
@@ -115,212 +134,11 @@ public:
     return x + w;
   }
 private:
+  int const shrinkTo = 20;
   int maxRight;
   int x, y;
   int w, h;
   int dx;
-};
-
-class Brick {
-public:
-  Brick(int Lives, int X, int Y, int W, int H) {
-    lives = Lives;
-    x = X;
-    y = Y;
-    w = W;
-    h = H;
-    bonus = Drop::nothing;
-  }
-  void _draw(QPainter* qp) {
-    qp->setBrush(color);
-    qp->drawRect(x, y, w, h);
-  }
-  virtual void draw(QPainter* qp) = 0;
-  void setBonus(Drop b) {
-    bonus = b;
-  }
-  Drop _loseLive() {
-    Drop temp = bonus;
-    bonus = Drop::nothing;
-    lives--;
-    changeColor();
-    return temp;
-  }
-  virtual Drop loseLive() {
-    return this->_loseLive();
-  }
-  void changeColor() {
-    switch (lives) {
-    case 1:
-      color = QColor("#FA8072");
-      break;
-    case 2:
-      color = QColor("#FF0000");
-      break;
-    case 3:
-      color = QColor("#B22222");
-      break;
-    case 4:
-      color = QColor("#8B0000");
-      break;
-    default:
-      break;
-    }
-  }
-  void setColor(QColor col) {
-    color = col;
-  }
-  void setColor() {
-    changeColor();
-  }
-  void setX(int X) {
-    x = X;
-  }
-  void changePos(int s) {
-    x += s;
-  }
-  virtual bool isAlive() {
-    return lives;
-  }
-  int top() {
-    return y;
-  }
-  int bottom() {
-    return y + h;
-  }
-  int left() {
-    return x;
-  }
-  int right() {
-    return x + w;
-  }
-private:
-  int x, y;
-  int w, h;
-  QColor color;
-  int lives;
-  Drop bonus;
-};
-
-class UnbreakableBrick : public Brick {
-public:
-  UnbreakableBrick(int lives, int x, int y, int w, int h) : Brick(lives, x, y, w, h) {
-    this->setColor();
-    this->setBonus(Drop::nothing);
-  }
-  bool isAlive() {
-    return true;
-  }
-  void draw(QPainter* qp) {
-    this->_draw(qp);
-  }
-};
-
-class SimpleBrick : public Brick {
-public:
-  SimpleBrick(int lives, int x, int y, int w, int h) : Brick(lives, x, y, w, h) {
-    this->setColor();
-    switch (rand() % 8) {
-    case 1:
-      this->setBonus(Drop::brick);
-      //this->setColor(QColor("blue"));
-      break;
-    case 2:
-      this->setBonus(Drop::newball);
-      //this->setColor(QColor("green"));
-      break;
-    default:
-      this->setBonus(Drop::nothing);
-      break;
-    }
-  }
-  void draw(QPainter* qp) {
-    qp->setPen(QColor("#FF8C00"));
-    this->_draw(qp);
-  }
-};
-
-class SpeedBrick : public Brick {
-public:
-  SpeedBrick(int lives, int x, int y, int w, int h) : Brick(lives, x, y, w, h) { 
-    this->setBonus(Drop::speed);
-  }
-  void draw(QPainter* qp) {
-    setColor(QColor("#FF1493"));
-    this->_draw(qp);
-  }
-  virtual Drop loseLive() {
-    this->_loseLive();
-    setBonus(Drop::speed);
-    return Drop::speed;
-  }
-};
-
-class MovingBrick : public Brick {
-public:
-  MovingBrick(int x, int y, int w, int h, int Pos) : Brick(3, x, y, w, h) {
-    this->setColor();
-    dx = 1;
-    pos = Pos;
-  }
-  void move() {
-    this->changePos(dx);
-  }
-  void goBack() {
-    dx = -dx;
-  }
-  int getPos() {
-    return pos;
-  }
-  void draw(QPainter* qp) {
-    this->_draw(qp);
-  }
-private:
-  int dx;
-  int pos;
-};
-
-class Bonus {
-public:
-  Bonus(Drop Type, int X, int Y) {
-    type = Type;
-    if (type == Drop::newball) 
-      color = QColor("green");
-    else if (type == Drop::brick)
-      color = QColor("red");
-    type = Type;
-    x = X;
-    y = Y;
-  }
-  Drop getType() {
-    return type;
-  }
-  void draw(QPainter* qp) {
-    qp->setBrush(color);
-    qp->drawRect(x - w/2, y, w, h);
-  }
-  void move() {
-    y += dy;
-  }
-  int top() {
-    return y;
-  }
-  int bottom() {
-    return y + h;
-  }
-  int left() {
-    return x;
-  }
-  int right() {
-    return x + w;
-  }
-private:
-  Drop type;
-  QColor color;
-  int const w = 10;
-  int const h = 10;
-  int const dy = 1;
-  int x, y;
 };
 
 class Arcanoid : public QWidget {
@@ -338,8 +156,12 @@ private:
   std::vector<Ball*> balls;
   std::vector<MovingBrick*> movingBricks;
   std::vector<Bonus*> bonuses;
+  std::vector<Paddle*> dno;
   Paddle* paddle;
   int timerId;
+  int bonuslives;
+  int lives;
+  int briksleft;
   void checkBrick(MovingBrick* brick);
   void checkEncounter();
   void checkColisions(Ball* ball);
@@ -349,12 +171,17 @@ private:
   void touchedBox(Ball* ball);
   bool touchedLR(Ball* ball, Brick* brick);
   bool touchedUD(Ball* ball, Brick* brick);
-  void startGame();
+  void encounter(Ball* a, Ball* b);
+  Ball* stickball;
+  int stick;
   int brickPos;
+  int tick;
+  int points;
   const int width = 500;
   const int height = 600;
   const int brickw = 50;
   const int brickh = 20;
   const int ballD = 16;
+  const int paddleh = 10;
 };
 
